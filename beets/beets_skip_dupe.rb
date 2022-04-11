8f70dfa5261c035b257eb041567f79629fb7d988
b08x <rwpannick@gmail.com>
2022-04-11 07:31:23 +0000

    added beets import skipped/remove duplicate script

---
+++ beets/beets_skip_dupe.rb
@@ -0,0 +1,53 @@
+#!/usr/bin/env ruby
+
+require 'pathname'
+require 'shellwords'
+require 'tty-command'
+
+def beet_import(folder)
+  cmd = TTY::Command.new(pty: false, printer: :pretty)
+  cmd.run("beet import '#{folder}'", :err => :out, env: {EDITOR: "nvim"})
+end
+
+beets_log = File.open("beet_log.txt")
+
+skipped_to_import = "to_import.txt"
+duplicates_to_remove = "to_remove.txt"
+
+skipped = []
+duplicate = []
+
+beets_log.each_line do |line|
+
+  folder = line.split(";")[0]
+
+  if folder.include?("duplicate")
+    duplicate << Pathname.new(File.join(folder.gsub("duplicate-skip ","").chomp)).dirname.to_s
+    next
+  end
+  
+  if folder.include?("skip")
+    skipped << Pathname.new(File.join(folder.gsub("skip ","").chomp)).dirname.to_s
+  end
+
+end
+
+skipped = skipped.uniq.map { |x| Pathname.new(x) }
+duplicate = duplicate.uniq.map { |x| Pathname.new(x) }
+
+skipped.each do |folder| 
+  File.open(skipped_to_import, 'a') do |f|
+    f << folder
+  end
+end
+
+duplicate.each do |folder|
+  File.open(duplicates_to_remove, 'a') do |f|
+    f << folder
+  end
+end
+
+
+#skipped.each do |folder|
+#  puts "#{folder.to_s.shellescape}"
+#end
