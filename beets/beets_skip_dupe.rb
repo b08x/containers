2b0a4b609163bfda742691113a32bdc2b32d2cd4
b08x <rwpannick@gmail.com>
2022-04-11 03:58:44 -0400

    adjusted script to track duplicate-skips

--- beets/beets_skip_dupe.rb
+++ beets/beets_skip_dupe.rb
@@ -22,12 +22,12 @@ beets_log.each_line do |line|
   folder = line.split(";")[0]
 
   if folder.include?("duplicate")
-    duplicate << Pathname.new(File.join(folder.gsub("duplicate-skip ","").chomp)).dirname.to_s
+    duplicate << Pathname.new(File.join(folder.gsub("duplicate-skip ","").chomp)).dirname.to_s + "\n"
     next
   end
-  
+
   if folder.include?("skip")
-    skipped << Pathname.new(File.join(folder.gsub("skip ","").chomp)).dirname.to_s
+    skipped << Pathname.new(File.join(folder.gsub("skip ","").chomp)).dirname.to_s + "\n"
   end
 
 end
@@ -35,9 +35,9 @@ end
 skipped = skipped.uniq.map { |x| Pathname.new(x) }
 duplicate = duplicate.uniq.map { |x| Pathname.new(x) }
 
-skipped.each do |folder| 
+skipped.each do |folder|
   File.open(skipped_to_import, 'a') do |f|
-    f << folder
+    f << folder.to_s.shellescape
   end
 end
 
@@ -47,6 +47,7 @@ duplicate.each do |folder|
   end
 end
 
+#OLDIFS=$IFS; IFS=$'\n'; for folder in $(cat to_import.txt); do beet import $folder; done
 
 #skipped.each do |folder|
 #  puts "#{folder.to_s.shellescape}"
